b085afebe3f101c9b7da7f9965746a4f988f7c8a
Raku Zeta <zetaraku@gmail.com>
2021-06-13 14:36:44 +0800

    ✨ use bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import * as Vue from 'vue';
 import router from './router';
 import App from './App.vue';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
 
 const app = Vue.createApp(App);
 
