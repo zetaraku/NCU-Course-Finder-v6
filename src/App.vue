bd5b07744ee9ede255506533c72ef2e806f98c2b
Raku Zeta <zetaraku@gmail.com>
2021-06-13 15:51:52 +0800

    🚚 move demo code into HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <router-view />
+  <div class="App">
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -12,12 +10,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,37 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a
-      href="https://vitejs.dev/guide/features.html"
-      target="_blank"
+  <div class="HelloWorld">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
     >
-      Vite Documentation
-    </a>
-    |
-    <a
-      href="https://v3.vuejs.org/"
-      target="_blank"
-    >Vue 3 Documentation</a>
-  </p>
 
-  <button
-    type="button"
-    @click="state.count++"
-  >
-    count is: {{ state.count }}
-  </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <h1>{{ msg }}</h1>
+
+    <p>
+      <a
+        href="https://vitejs.dev/guide/features.html"
+        target="_blank"
+      >
+        Vite Documentation
+      </a>
+      |
+      <a
+        href="https://v3.vuejs.org/"
+        target="_blank"
+      >Vue 3 Documentation</a>
+    </p>
+
+    <button
+      type="button"
+      @click="state.count++"
+    >
+      count is: {{ state.count }}
+    </button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
 </template>
 
 <script>
@@ -50,5 +57,13 @@ export default {
 <style scoped>
 a {
   color: #42b983;
+}
+.HelloWorld {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
