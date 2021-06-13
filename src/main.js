feea4158e5c7b413acdbf0c7289c873dfb791f65
Raku Zeta <zetaraku@gmail.com>
2021-06-13 16:28:15 +0800

    💄 customize bootstrap styles

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import * as Vue from 'vue';
 import router from './router';
 import App from './App.vue';
 import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/scss/bootstrap.scss';
+import './styles/bootstrap-custom.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = Vue.createApp(App);
