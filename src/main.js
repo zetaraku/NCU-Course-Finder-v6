3e0dd96d11930d9df297453442c814e563678751
Raku Zeta <zetaraku@gmail.com>
2021-06-16 21:04:14 +0800

    ✨ use v-tooltip

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import * as Vue from 'vue';
 import VueGtagPlugin from 'vue-gtag-next';
+import VTooltipPlugin from 'v-tooltip';
 import store from './store';
 import router from './router';
 import App from './App.vue';
 import 'bootstrap/dist/js/bootstrap';
 import './styles/bootstrap-custom.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'v-tooltip/dist/v-tooltip.css';
 import './styles/style.scss';
 
 const app = Vue.createApp(App);
@@ -17,5 +19,6 @@ app.use(VueGtagPlugin, {
     id: import.meta.env.VITE_GTAG_ID,
   },
 });
+app.use(VTooltipPlugin);
 
 app.mount('#app');
