5b697669690e81b48b5f56d2701bbb8eb9d385ae
Raku Zeta <zetaraku@gmail.com>
2021-06-16 20:39:13 +0800

    ✨ add course columns data

---
+++ src/components/course-finder/table-view/columns/data.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+export const columns = [
+  { name: '課號', key: 'classNo' },
+  { name: '課程名稱', key: 'title' },
+  { name: '授課教師', key: 'teachers' },
+  { name: '學分', key: 'credit' },
+  { name: '中選率', key: 'successRate' },
+  { name: '飽和度', key: 'fullRate' },
+  { name: '上課時段', key: 'classTimes' },
+];
