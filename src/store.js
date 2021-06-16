968ab8cdc51c1d1b0dcd340c96614130d249d136
Raku Zeta <zetaraku@gmail.com>
2021-06-16 12:26:00 +0800

    ✨ add states and mutations for course data

--- src/store.js
+++ src/store.js
@@ -4,9 +4,29 @@ import * as Vuex from 'vuex';
 const store = Vuex.createStore({
   state() {
     return {
+      colleges: [],
+      departments: [],
+      courses: [],
+      lastUpdateTime: null,
+      errorMessage: null,
     };
   },
   mutations: {
+    SET_COLLEGES(state, payload) {
+      state.colleges = payload.colleges;
+    },
+    SET_DEPARTMENTS(state, payload) {
+      state.departments = payload.departments;
+    },
+    SET_COURSES(state, payload) {
+      state.courses = payload.courses;
+    },
+    SET_LAST_UPDATE_TIME(state, payload) {
+      state.lastUpdateTime = payload.lastUpdateTime;
+    },
+    SET_ERROR_MESSAGE(state, payload) {
+      state.errorMessage = payload.errorMessage;
+    },
   },
   actions: {
   },
