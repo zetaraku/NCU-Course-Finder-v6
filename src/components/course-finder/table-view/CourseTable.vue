0270b159c5ebe79d09fea98e64ed4b2be6446b6e
Raku Zeta <zetaraku@gmail.com>
2021-06-16 20:15:18 +0800

    ✨ add CourseTable

---
+++ src/components/course-finder/table-view/CourseTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <table class="CourseTable table table-hover text-center align-middle">
+    <thead>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    courses: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    return {
+    };
+  },
+};
+</script>
--- src/views/CourseFinder.vue
+++ src/views/CourseFinder.vue
@@ -5,6 +5,10 @@
       :error-message="errorMessage"
       class="my-4"
     />
+    <CourseTable
+      :courses="coursesInCurrentPage"
+      class="my-4"
+    />
   </div>
 </template>
 
@@ -12,20 +16,30 @@
 import * as Vue from 'vue';
 import * as Vuex from 'vuex';
 import DataStatusIndicator from '@/components/DataStatusIndicator.vue';
+import CourseTable from '@/components/course-finder/table-view/CourseTable.vue';
 
 export default {
   components: {
     DataStatusIndicator,
+    CourseTable,
   },
   setup() {
     const store = Vuex.useStore();
 
+    const courses = Vue.computed(() => store.state.courses);
     const lastUpdateTime = Vue.computed(() => store.state.lastUpdateTime);
     const errorMessage = Vue.computed(() => store.state.errorMessage);
 
+    const coursesInCurrentPage = Vue.computed(
+      () => courses.value.slice(0, 24),
+    );
+
     return {
+      courses,
       lastUpdateTime,
       errorMessage,
+
+      coursesInCurrentPage,
     };
   },
 };
