c744e3730bd48f1100a2d549972e45d860ab6931
Raku Zeta <zetaraku@gmail.com>
2021-06-16 19:47:13 +0800

    ✨ add DataStatusIndicator

---
+++ src/components/DataStatusIndicator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="DataStatusIndicator">
+    <div
+      v-if="errorMessage !== null"
+      class="alert alert-danger"
+    >
+      <i class="bi bi-exclamation-circle me-1" />
+      <span>
+        取得資料時發生了錯誤，請回報管理員。 (錯誤訊息：{{ errorMessage }})
+      </span>
+    </div>
+    <div
+      v-else-if="lastUpdateTime === null"
+      class="alert alert-info"
+    >
+      <i class="bi bi-clock me-1" />
+      <span>
+        取得資料中 ... 請稍候 ...
+      </span>
+    </div>
+    <div
+      v-else
+      class="alert alert-secondary alert-dismissible fade show"
+    >
+      <i class="bi bi-info-circle me-1" />
+      <span>
+        更新<span class="d-none d-sm-inline">時間</span>：{{ formatDateTime(lastUpdateTime) }}
+        ({{ formatRelativeTime(lastUpdateTime) }})
+      </span>
+      <button
+        class="btn-close"
+        data-bs-dismiss="alert"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatDateTime, formatRelativeTime } from '@/helpers';
+
+export default {
+  props: {
+    lastUpdateTime: {
+      type: Date,
+      default: null,
+    },
+    errorMessage: {
+      type: String,
+      default: null,
+    },
+  },
+  setup() {
+    return {
+      formatDateTime,
+      formatRelativeTime,
+    };
+  },
+};
+</script>
--- src/views/CourseFinder.vue
+++ src/views/CourseFinder.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="CourseFinder text-center">
-    <h1>CourseFinder</h1>
+  <div class="CourseFinder container my-4">
+    <DataStatusIndicator
+      :last-update-time="lastUpdateTime"
+      :error-message="errorMessage"
+      class="my-4"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import * as Vue from 'vue';
+import * as Vuex from 'vuex';
+import DataStatusIndicator from '@/components/DataStatusIndicator.vue';
 
+export default {
+  components: {
+    DataStatusIndicator,
+  },
+  setup() {
+    const store = Vuex.useStore();
+
+    const lastUpdateTime = Vue.computed(() => store.state.lastUpdateTime);
+    const errorMessage = Vue.computed(() => store.state.errorMessage);
+
+    return {
+      lastUpdateTime,
+      errorMessage,
+    };
+  },
 };
 </script>
 
