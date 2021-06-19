5cceed57dc08cb995d05c47075fddf69506757e4
Raku Zeta <zetaraku@gmail.com>
2021-06-19 23:59:47 +0800

    🎨 move table into div

--- src/components/CourseFinder.vue/CourseTable.vue/CourseTable.vue
+++ src/components/CourseFinder.vue/CourseTable.vue/CourseTable.vue
@@ -1,17 +1,19 @@
 <template>
-  <table class="CourseTable table table-hover text-center align-middle">
-    <thead>
-      <CourseTableHeader :columns="columns" />
-    </thead>
-    <tbody>
-      <CourseTableRow
-        v-for="course in courses"
-        :key="course.serialNo"
-        :columns="columns"
-        :course="course"
-      />
-    </tbody>
-  </table>
+  <div class="CourseTable">
+    <table class="table table-hover text-center align-middle">
+      <thead>
+        <CourseTableHeader :columns="columns" />
+      </thead>
+      <tbody>
+        <CourseTableRow
+          v-for="course in courses"
+          :key="course.serialNo"
+          :columns="columns"
+          :course="course"
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
