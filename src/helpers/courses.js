50ad769bfab0721483175cd6909c6e7796d80167
Raku Zeta <zetaraku@gmail.com>
2021-06-19 22:20:26 +0800

    ✨ add courses filter helper

--- src/helpers/courses.js
+++ src/helpers/courses.js
@@ -1,4 +1,77 @@
-// eslint-disable-next-line import/prefer-default-export
+export function filterCourses(courses, filters) {
+  let results = courses.slice();
+
+  if (filters.collegeId !== null) {
+    results = results.filter(
+      course => course.collegeIds.includes(filters.collegeId),
+    );
+  }
+  if (filters.departmentId !== null) {
+    results = results.filter(
+      course => course.departmentIds.includes(filters.departmentId),
+    );
+  }
+  if (filters.classPrefix !== null) {
+    results = results.filter(
+      course => course.classNo.startsWith(filters.classPrefix),
+    );
+  }
+  if (filters.title !== '') {
+    const keywords = filters.title.split(/\s+/);
+    results = results.filter(
+      course => keywords.some(
+        keyword => course.title.includes(keyword),
+      ),
+    );
+  }
+  if (filters.teachers !== '') {
+    const keywords = filters.teachers.split(/\s+/);
+    results = results.filter(
+      course => course.teachers.some(
+        teacher => keywords.some(
+          keyword => teacher.includes(keyword),
+        ),
+      ),
+    );
+  }
+  if (filters.classNo !== '') {
+    results = results.filter(
+      course => course.classNo.includes(filters.classNo),
+    );
+  }
+  if (filters.classTimes.length > 0) {
+    results = results.filter(
+      course => course.classTimes.some(
+        classTime => filters.classTimes.includes(classTime),
+      ),
+    );
+  }
+  if (filters.credits.length > 0) {
+    results = results.filter(
+      course => filters.credits.includes(course.credit),
+    );
+  }
+  if (filters.courseType !== null) {
+    results = results.filter(
+      course => course.courseType === filters.courseType,
+    );
+  }
+  if (filters.passwordCard !== null) {
+    results = results.filter(
+      course => course.passwordCard === filters.passwordCard,
+    );
+  }
+  if (filters.extraOptions.length > 0) {
+    results = results.filter(
+      course => filters.extraOptions.every(
+        predicate => predicate(course) === true,
+      ),
+    );
+  }
+
+  return results;
+}
+
 export function sortCourses(courses, sorting) {
   let result = courses.slice();
 
