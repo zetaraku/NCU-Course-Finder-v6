b0acda35df196dd2571f3d218f3f3c1ce1411f4a
Raku Zeta <zetaraku@gmail.com>
2021-06-22 16:58:24 +0800

    🚚 move columns data

--- src/components/CourseFinder.vue/CourseTable.vue/CourseTable.vue
+++ src/components/CourseFinder.vue/CourseTable.vue/CourseTable.vue
@@ -19,7 +19,7 @@
 <script>
 import CourseTableHeader from './CourseTableHeader.vue';
 import CourseTableRow from './CourseTableRow.vue';
-import { columns } from './columns/data';
+import { columns } from './Col/data';
 
 export default {
   components: {
