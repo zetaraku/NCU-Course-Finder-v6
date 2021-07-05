67bfb141af1547044b907e0f02cd310355264b0b
Raku Zeta <zetaraku@gmail.com>
2021-07-05 23:11:34 +0800

    ✨ add CourseGridHeader

--- src/components/CourseFinder.vue/CourseGrid.vue/CourseGrid.vue
+++ src/components/CourseFinder.vue/CourseGrid.vue/CourseGrid.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="CourseGrid">
+    <CourseGridHeader class="mb-4" />
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
       <div
         v-for="course in courses"
@@ -16,10 +17,12 @@
 </template>
 
 <script>
+import CourseGridHeader from './CourseGridHeader.vue';
 import CourseGridTile from './CourseGridTile.vue';
 
 export default {
   components: {
+    CourseGridHeader,
     CourseGridTile,
   },
   props: {
---
+++ src/components/CourseFinder.vue/CourseGrid.vue/CourseGridHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="CourseGridHeader row align-items-center">
+    <div class="col col-auto">
+      <label class="fw-bold">
+        排序依據
+      </label>
+    </div>
+    <div class="col">
+      <select
+        v-model="sorting.column"
+        class="form-select"
+      >
+        <option :value="null">
+          【預設排序】
+        </option>
+        <option
+          v-for="column in columns"
+          :key="column.key"
+          :value="column"
+        >
+          {{ column.name }}
+        </option>
+      </select>
+    </div>
+    <div class="col col-auto">
+      <button
+        class="btn btn-light border border-2"
+        @click="toggleSortingOrder"
+      >
+        <template v-if="sorting.order === 'asc'">
+          <i class="bi bi-sort-up-alt me-1" />
+          升序
+        </template>
+        <template v-else>
+          <i class="bi bi-sort-down me-1" />
+          降序
+        </template>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Vue from 'vue';
+import { columns } from '../CourseTable.vue/ColComponents/data';
+
+export default {
+  setup() {
+    const sorting = Vue.inject('sorting');
+
+    return {
+      columns,
+      sorting,
+
+      toggleSortingOrder() {
+        sorting.value.order = sorting.value.order === 'asc' ? 'desc' : 'asc';
+      },
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
