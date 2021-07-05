8b687376240f1e248b980208905feb70bfee1459
Raku Zeta <zetaraku@gmail.com>
2021-07-05 22:58:32 +0800

    ✨ add CourseGridTile

--- src/components/CourseFinder.vue/CourseGrid.vue/CourseGrid.vue
+++ src/components/CourseFinder.vue/CourseGrid.vue/CourseGrid.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="CourseGrid">
-    <!-- CourseGrid body -->
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
+      <div
+        v-for="course in courses"
+        :key="course.serialNo"
+        class="col"
+      >
+        <CourseGridTile
+          :course="course"
+          class="h-100"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CourseGridTile from './CourseGridTile.vue';
+
 export default {
+  components: {
+    CourseGridTile,
+  },
   props: {
     courses: {
       type: Array,
---
+++ src/components/CourseFinder.vue/CourseGrid.vue/CourseGridTile.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="CourseGridTile card position-relative"
+    :class="{ 'bg-warning': course.selected }"
+  >
+    <div class="card-body row flex-column align-content-start text-center gy-2">
+      <div class="d-flex justify-content-center">
+        <ColCourseType class="me-2" />
+        <ColClassNo />
+      </div>
+      <div>
+        <ColTitle />
+      </div>
+      <div>
+        <ColTeachers />
+      </div>
+      <div>
+        <ColCredit />
+      </div>
+      <div>
+        <ColSuccessRate />
+      </div>
+      <div>
+        <ColFullRate />
+      </div>
+      <div class="flex-grow-1 flex-centered mt-3">
+        <ColClassTime />
+      </div>
+      <div class="m-0">
+        <label class="position-absolute top-0 end-0 px-2 py-1 m-sm-1 cursor-pointer">
+          <ColSelected />
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Vue from 'vue';
+import * as ColComponents from '../CourseTable.vue/ColComponents';
+
+export default {
+  components: {
+    ...ColComponents,
+  },
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    Vue.provide('course', props.course);
+  },
+};
+</script>
