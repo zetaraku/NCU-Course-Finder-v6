8668212f3e1c1d040e39c49b677eba43b7ccc1e6
Raku Zeta <zetaraku@gmail.com>
2021-07-05 15:25:10 +0800

    ✨ display selected classTime with red color

--- src/components/CourseFinder.vue/CourseTable.vue/ColComponents/ColClassTime.vue
+++ src/components/CourseFinder.vue/CourseTable.vue/ColComponents/ColClassTime.vue
@@ -3,7 +3,8 @@
     <span
       v-for="classTime in course.classTimes"
       :key="classTime"
-      class="badge rounded-pill bg-primary font-monospace m-1"
+      class="badge rounded-pill font-monospace m-1"
+      :class="selectedClassTimes.has(classTime) ? 'bg-danger' : 'bg-primary'"
     >
       {{ classTime }}
     </span>
@@ -17,7 +18,18 @@
 </template>
 
 <script>
+import * as Vue from 'vue';
+import * as Vuex from 'vuex';
+
 export default {
   inject: ['course'],
+  setup() {
+    const store = Vuex.useStore();
+    const selectedClassTimes = Vue.computed(() => store.getters.selectedClassTimes);
+
+    return {
+      selectedClassTimes,
+    };
+  },
 };
 </script>
