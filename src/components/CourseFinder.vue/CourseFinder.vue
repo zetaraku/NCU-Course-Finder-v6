47f470e2dfa0fde70963a066b32c0385171d99be
Raku Zeta <zetaraku@gmail.com>
2021-07-14 23:04:17 +0800

    ♻️ extract FilteringControls

--- src/components/CourseFinder.vue/CourseFinder.vue
+++ src/components/CourseFinder.vue/CourseFinder.vue
@@ -6,37 +6,11 @@
     <CourseFilter
       class="my-4"
     />
-    <div class="d-flex justify-content-center my-5">
-      <div class="d-flex flex-column align-items-center mx-2 mx-sm-3">
-        <button
-          class="btn btn-lg btn-secondary text-nowrap px-sm-4"
-          @click="resetFilters"
-        >
-          重置篩選條件
-        </button>
-      </div>
-      <div class="d-flex flex-column align-items-center mx-2 mx-sm-3">
-        <button
-          class="btn btn-lg btn-primary text-nowrap px-sm-4"
-          :disabled="autoFilteringEnabled"
-          @click="refreshFilteredCourses"
-        >
-          開始多重篩選
-        </button>
-        <label
-          v-tooltip.bottom="'※如果在變更篩選條件時延遲過久，可試著取消勾選此選項並改為手動套用'"
-          class="form-check-label text-nowrap user-select-none cursor-pointer mt-3"
-        >
-          <input
-            v-model="autoFilteringEnabled"
-            type="checkbox"
-            class="form-check-input cursor-pointer m-1"
-          >
-          自動套用
-          <i class="bi bi-info-circle ms-1" />
-        </label>
-      </div>
-    </div>
+    <FilteringControls
+      class="my-5"
+      @reset-filters="resetFilters"
+      @refresh-filtered-courses="refreshFilteredCourses"
+    />
     <ResultIndicator
       :value="100 * filteredCourses.length / courses.length"
       class="my-5"
@@ -57,6 +31,7 @@ import * as Vuex from 'vuex';
 import { makeFilters, filterCourses } from '@/helpers';
 import DataStatusIndicator from './DataStatusIndicator.vue';
 import CourseFilter from './CourseFilter.vue';
+import FilteringControls from './FilteringControls.vue';
 import ResultIndicator from './ResultIndicator.vue';
 import CourseView from './CourseView.vue';
 
@@ -64,6 +39,7 @@ export default {
   components: {
     DataStatusIndicator,
     CourseFilter,
+    FilteringControls,
     ResultIndicator,
     CourseView,
   },
@@ -107,10 +83,9 @@ export default {
     return {
       courses,
       filteredCourses,
-      autoFilteringEnabled,
 
-      refreshFilteredCourses,
       resetFilters,
+      refreshFilteredCourses,
     };
   },
 };
---
+++ src/components/CourseFinder.vue/FilteringControls.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="FilteringControls d-flex justify-content-center">
+    <div class="d-flex flex-column align-items-center mx-2 mx-sm-3">
+      <button
+        class="btn btn-lg btn-secondary text-nowrap px-sm-4"
+        @click="$emit('reset-filters');"
+      >
+        重置篩選條件
+      </button>
+    </div>
+    <div class="d-flex flex-column align-items-center mx-2 mx-sm-3">
+      <button
+        class="btn btn-lg btn-primary text-nowrap px-sm-4"
+        :disabled="autoFilteringEnabled"
+        @click="$emit('refresh-filtered-courses');"
+      >
+        開始多重篩選
+      </button>
+      <label
+        v-tooltip.bottom="'※如果在變更篩選條件時延遲過久，可試著取消勾選此選項並改為手動套用'"
+        class="form-check-label text-nowrap user-select-none cursor-pointer mt-3"
+      >
+        <input
+          v-model="autoFilteringEnabled"
+          type="checkbox"
+          class="form-check-input cursor-pointer m-1"
+        >
+        自動套用
+        <i class="bi bi-info-circle ms-1" />
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Vue from 'vue';
+
+export default {
+  emits: [
+    'reset-filters',
+    'refresh-filtered-courses',
+  ],
+  setup() {
+    const autoFilteringEnabled = Vue.inject('autoFilteringEnabled');
+
+    return {
+      autoFilteringEnabled,
+    };
+  },
+};
+</script>
