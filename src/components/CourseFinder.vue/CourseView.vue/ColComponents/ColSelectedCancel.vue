9304f3e24e91691d17be7251e77d97bd4914be68
Raku Zeta <zetaraku@gmail.com>
2021-07-15 12:00:02 +0800

    🩹 use bootstrap built-in colors

--- src/components/CourseFinder.vue/CourseView.vue/ColComponents/ColSelectedCancel.vue
+++ src/components/CourseFinder.vue/CourseView.vue/ColComponents/ColSelectedCancel.vue
@@ -35,8 +35,8 @@ export default {
 <style lang="scss" scoped>
 .form-check-input[type="checkbox"] {
   &.form-check-input-cancel:checked {
-    background-color: #dc3545;
-    border-color: #dc3545;
+    background-color: var(--bs-danger);
+    border-color: var(--bs-danger);
     background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3e%3cpath fill='none' stroke='%23fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='3' d='M6 6l8 8M14 6l-8 8'/%3e%3c/svg%3e");
   }
 }
