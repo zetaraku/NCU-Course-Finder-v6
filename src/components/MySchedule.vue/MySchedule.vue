cd7cbe9f71d0975abc8e6e8fc1e421ab58e91d91
Raku Zeta <zetaraku@gmail.com>
2021-07-18 21:51:51 +0800

    ✨ add MyScheduleHeader

--- src/components/MySchedule.vue/MySchedule.vue
+++ src/components/MySchedule.vue/MySchedule.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="MySchedule container my-4">
+    <MyScheduleHeader />
   </div>
 </template>
 
 <script>
+import MyScheduleHeader from './MyScheduleHeader.vue';
+
 export default {
+  components: {
+    MyScheduleHeader,
+  },
 };
 </script>
---
+++ src/components/MySchedule.vue/MyScheduleHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="MyScheduleHeader position-relative mb-5">
+    <h1 class="text-center">
+      我的課表
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+  },
+};
+</script>
