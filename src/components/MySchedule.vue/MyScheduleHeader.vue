43846e9c6de6dd30a97b03e18d85082a71e89ed8
Raku Zeta <zetaraku@gmail.com>
2021-07-18 21:55:01 +0800

    ✨ add clearSchedule and printSchedule button

--- src/components/MySchedule.vue/MyScheduleHeader.vue
+++ src/components/MySchedule.vue/MyScheduleHeader.vue
@@ -1,14 +1,48 @@
 <template>
   <div class="MyScheduleHeader position-relative mb-5">
+    <button
+      class="btn btn-outline-danger btn-sm position-absolute bottom-0 start-0 d-print-none"
+      @click="clearSchedule"
+    >
+      <i class="bi bi-trash me-1" />
+      清空<span class="d-none d-sm-inline">課表</span>
+    </button>
     <h1 class="text-center">
       我的課表
     </h1>
+    <button
+      class="btn btn-outline-primary btn-sm position-absolute bottom-0 end-0 d-print-none"
+      @click="printSchedule"
+    >
+      <i class="bi bi-printer me-1" />
+      列印<span class="d-none d-sm-inline">課表</span>
+    </button>
   </div>
 </template>
 
 <script>
+import * as Vue from 'vue';
+import * as Vuex from 'vuex';
+import * as VueGtag from 'vue-gtag-next';
+
 export default {
   setup() {
+    const store = Vuex.useStore();
+    const gtag = VueGtag.useGtag();
+
+    const courses = Vue.computed(() => store.state.courses);
+
+    return {
+      clearSchedule() {
+        for (let course of courses.value) {
+          course.selected = false;
+        }
+      },
+      printSchedule() {
+        window.print();
+        gtag.event('PrintSchedule');
+      },
+    };
   },
 };
 </script>
