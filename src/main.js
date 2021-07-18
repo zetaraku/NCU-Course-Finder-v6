ec763477b1bd2658eee0165393820ab2b5ff23dd
Raku Zeta <zetaraku@gmail.com>
2021-07-18 17:22:17 +0800

    🔊 fire alert on unhandled promise rejection

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'v-tooltip/dist/v-tooltip.css';
 import './styles/style.scss';
 
+window.addEventListener('unhandledrejection', event => {
+  // eslint-disable-next-line no-alert
+  window.alert(event.reason);
+});
+
 const app = Vue.createApp(App);
 
 app.use(store);
