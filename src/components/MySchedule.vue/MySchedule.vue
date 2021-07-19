b7dc359e04f4cf500971aea0da57134a01241291
Raku Zeta <zetaraku@gmail.com>
2021-07-19 16:05:03 +0800

    ✨ add print style for MySchedule

--- src/components/MySchedule.vue/MySchedule.vue
+++ src/components/MySchedule.vue/MySchedule.vue
@@ -16,3 +16,43 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap') print;
+
+@media print {
+  @page {
+    size: A4 portrait;
+  }
+
+  * {
+    -webkit-print-color-adjust: exact !important;
+    color-adjust: exact !important;
+  }
+
+  :root {
+    --bs-font-sans-serif: 'Noto Sans TC', sans-serif !important;
+  }
+
+  html, body {
+    height: 100vh;
+    margin: 0 !important;
+    padding: 0 !important;
+    overflow: hidden;
+  }
+
+  header {
+    display: none !important;
+  }
+  main {
+    padding: 0 !important;
+  }
+  footer {
+    display: none !important;
+  }
+
+  .MySchedule {
+    max-width: 100% !important;
+  }
+}
+</style>
