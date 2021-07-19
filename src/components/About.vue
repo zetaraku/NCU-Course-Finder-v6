7bb004de20e9a20b1114b7031d62a2b2928363cb
Raku Zeta <zetaraku@gmail.com>
2021-07-19 16:19:14 +0800

    📝 update About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,13 +5,22 @@
         關於 NCU Course Finder
       </h1><hr>
     </div>
+    <figure class="text-center mt-4">
+      <blockquote class="blockquote">
+        <p>選課的季節又到了，<br class="d-sm-none">你可曾聽說過那名為<b>加退選</b>的戰爭？</p>
+      </blockquote>
+      <figcaption class="blockquote-footer mt-0">
+        <cite title="NCU Course Finder">NCU Course Finder</cite>,<br class="d-sm-none">
+        a rescue from the place where Nobody Care U.
+      </figcaption>
+    </figure>
     <div class="my-4">
       <h2>作者</h2>
       <p>
         <i class="bi bi-facebook me-2" />
         <a href="https://www.facebook.com/zt.raku/">Shu Huan Huang</a>
         <span>
-          － Hey! It's me, an unemployed violent psychopath who knows where you live >:)
+          － Hey! It's me, an ungraduated unemployed violent psychopath who knows where you live >:)
         </span>
       </p>
     </div>
@@ -21,7 +30,7 @@
         <i class="bi bi-facebook me-2" />
         <a href="https://www.facebook.com/NCUCourseFinder">NCU Course Finder</a>
         <span>
-          － 你可以在這裡<b>回報問題</b>或<b>提供建議</b>，看在作者花了上百個小時在這專案上幫你省下的時間，幫我<b>按個讚</b>嘛 :)
+          － 你可以在這裡<b>回報問題</b>或是<b>提供建議</b>，看在作者花了上百個小時在這個專案上幫你省下的時間，幫我<b>按個讚</b>嘛 :)
         </span>
       </p>
     </div>
@@ -33,7 +42,19 @@
         <span>
           － 本專案以
           <a href="https://zh.wikipedia.org/wiki/MIT%E8%A8%B1%E5%8F%AF%E8%AD%89">MIT授權條款</a>
-          授權開源，你可以在這邊找到完整的原始碼，如果你喜歡這個工具，可以到這裡幫我按個星星 ⭐ :)
+          授權開源，你可以在這邊找到完整的原始碼，如果你有興趣，也可以一起來改善這個專案 ⭐ :)
+        </span>
+      </p>
+    </div>
+    <div class="my-4">
+      <h2>之前的版本</h2>
+      <p>
+        <i class="bi bi-github me-2" />
+        <a href="https://github.com/zetaraku/NCU-Course-Finder-v4">NCU-Course-Finder-v4</a>
+        <span>
+          － 這個版本已經終止開發，但資料還是會同步更新，如果你比較習慣這個版本，可以前往
+          <a href="https://ncucf-v4.zetaraku.dev/">這裡</a>
+          繼續使用 :)
         </span>
       </p>
     </div>
@@ -44,6 +65,14 @@
         <a href="https://paypal.me/zetaraku">PayPal.Me</a>
         <span>
           － 如果你有多餘的錢也不是不可以贊助給我啦 ;)
+        </span>
+      </p>
+    </div>
+    <div class="my-4 text-white">
+      <h2>捉迷藏</h2>
+      <p>
+        <span>
+          － 你找到我了 :D
         </span>
       </p>
     </div>
