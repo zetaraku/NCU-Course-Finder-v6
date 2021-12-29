c17deff9f273dac2c8585a7acb32aea311a022d6
Raku Zeta <zetaraku@gmail.com>
2021-12-29 22:01:49 +0800

    🩹 fix build warning: "@charset" must be the first rule in the file

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+      },
+    },
+  },
 });
