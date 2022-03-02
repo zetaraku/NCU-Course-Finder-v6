3e40362ed157a41331b018330b30308da111da3d
Raku Zeta <zetaraku@gmail.com>
2022-03-02 19:03:15 +0800

    🚸 add hint to add courses in MySchedule

--- src/components/MySchedule.vue/MyScheduleHeader.vue
+++ src/components/MySchedule.vue/MyScheduleHeader.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="MyScheduleHeader position-relative mb-5">
-    <button
-      class="btn btn-outline-danger btn-sm position-absolute bottom-0 start-0 d-print-none"
-      @click="clearSchedule"
-    >
-      <i class="bi bi-trash me-1" />
-      清空<span class="d-none d-sm-inline">課表</span>
-    </button>
-    <h1 class="text-center">
-      我的課表
-    </h1>
-    <button
-      class="btn btn-outline-primary btn-sm position-absolute bottom-0 end-0 d-print-none"
-      @click="printSchedule"
-    >
-      <i class="bi bi-printer me-1" />
-      列印<span class="d-none d-sm-inline">課表</span>
-    </button>
+  <div class="MyScheduleHeader mb-5">
+    <div class="position-relative">
+      <button
+        class="btn btn-outline-danger btn-sm position-absolute bottom-0 start-0 d-print-none"
+        @click="clearSchedule"
+      >
+        <i class="bi bi-trash me-1" />
+        清空<span class="d-none d-sm-inline">課表</span>
+      </button>
+      <h1 class="text-center">
+        我的課表
+      </h1>
+      <button
+        class="btn btn-outline-primary btn-sm position-absolute bottom-0 end-0 d-print-none"
+        @click="printSchedule"
+      >
+        <i class="bi bi-printer me-1" />
+        列印<span class="d-none d-sm-inline">課表</span>
+      </button>
+    </div>
+    <p class="text-center text-secondary mt-3 d-print-none">
+      ※你可以在課程查詢頁勾選課程來將課程加入我的課表
+    </p>
   </div>
 </template>
 
