e6efa649e245d5c111d6e0f16529a90b158ea27c
Raku Zeta <zetaraku@gmail.com>
2024-08-03 01:43:03 +0800

    🔧 inject process.env with vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          ...process.env,
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
